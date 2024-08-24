2fb877d6c5ac4a156bce824fef400d026c654514
Siri <67231494+Li-Siri2024@users.noreply.github.com>
2024-08-25 02:38:00 +0800

    删除无用文件

--- Omni/Applications/Software/control_setting_2.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROL_SETTING_H
-#define CONTROL_SETTING_H
-
-void remote_control_task(void);
-extern float auto_yaw,auto_pitch,last_yaw,last_pitch,visual_yaw;
-#define YAW_DATA_HIS 1
-#define PITCH_DATA_HIS 0
-
-#endif /* CONTROL_SETTING_H */
